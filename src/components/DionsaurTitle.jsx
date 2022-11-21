39b923e638764e343ae9f1d055bbceb36f9e0752
Yubo Zhang <a.yubozhang@gmail.com>
2022-11-20 22:40:55 -0800

    Moved components inside the components directory.
    Added dinosaur of the day components

---
+++ src/components/DionsaurTitle.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+function DinosaurTitle() {
+    return (
+        <div>
+            <h1>Dinosaur of the Day</h1>
+            <h3>Dinodinho the second</h3>
+            <div>
+                <image src='/public/dino.jpg' alt="image" />
+            </div>
+        </div>
+    )
+}
+
+export default DinosaurTitle;
