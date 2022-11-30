aa583f17c5cc5d6c3d2c4c403fa43738f046953b
Jinyuan Zhang <martinzhang0314@163.com>
2022-11-29 21:21:16 -0800

    This version include choice of all four parts of dinosaurs. Each part has nine choices. But the final render page hasn't created yet.

--- client/src/BuildYourOwn.js
+++ client/src/BuildYourOwn.js
@@ -4,15 +4,47 @@ import styles from "./general.module.css";
 import ReactDOM from 'react-dom/client';
 import './css/build-your-own.css';
 
-import leg_1 from './1.png';
-import leg_2 from './2.png';
-import leg_3 from './3.png';
-import leg_4 from './4.png';
-import leg_5 from './5.png';
-import leg_6 from './6.png';
-import leg_7 from './7.png';
-import leg_8 from './8.png';
-import leg_9 from './9.png';
+import leg_1 from './Leg/1.png';
+import leg_2 from './Leg/2.png';
+import leg_3 from './Leg/3.png';
+import leg_4 from './Leg/4.png';
+import leg_5 from './Leg/5.png';
+import leg_6 from './Leg/6.png';
+import leg_7 from './Leg/7.png';
+import leg_8 from './Leg/8.png';
+import leg_9 from './Leg/9.png';
+
+import head_1 from './Head/1.png';
+import head_2 from './Head/2.png';
+import head_3 from './Head/3.png';
+import head_4 from './Head/4.png';
+import head_5 from './Head/5.png';
+import head_6 from './Head/6.png';
+import head_7 from './Head/7.png';
+import head_8 from './Head/8.png';
+import head_9 from './Head/9.png';
+
+import body_1 from './Body/1.png';
+import body_2 from './Body/2.png';
+import body_3 from './Body/3.png';
+import body_4 from './Body/4.png';
+import body_5 from './Body/5.png';
+import body_6 from './Body/6.png';
+import body_7 from './Body/7.png';
+import body_8 from './Body/8.png';
+import body_9 from './Body/9.png';
+
+import tail_1 from './Tail/1.png';
+import tail_2 from './Tail/2.png';
+import tail_3 from './Tail/3.png';
+import tail_4 from './Tail/4.png';
+import tail_5 from './Tail/5.png';
+import tail_6 from './Tail/6.png';
+import tail_7 from './Tail/7.png';
+import tail_8 from './Tail/8.png';
+import tail_9 from './Tail/9.png';
+
+
 
 
 function Image(props) {
@@ -29,12 +61,12 @@ function Image(props) {
   );
 }
 
-class Choice extends React.Component {
+class Leg_Choice extends React.Component {
   constructor(props){
     super(props);
     this.state = {
 	index: Array(9).fill(null),
-	url: leg_1,
+	leg_url: [leg_1,leg_2,leg_3,leg_4,leg_5,leg_6,leg_7,leg_8,leg_9],
 	mark: 'X',
 	last: -1,
 	
@@ -53,26 +85,177 @@ class Choice extends React.Component {
       
   }
 
-  renderImage(i) {
-      return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.url}/>);
-  }
-
+    renderLeg(i) {
+	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.leg_url[i]}/>);
+    }
     render() {
-	const status = "Choose from below: ";
-	
+	const status = "Choose from below";
+	const leg = "Legs: "
 	return (
 	    <div>
 		<div className="status">{status}</div>
+		<div className="title">{leg}</div>
 		<div className="choice-row">
-		    {this.renderImage(0)}
-		    {this.renderImage(1)}
-		    {this.renderImage(2)}
-		    {this.renderImage(3)}
-		    {this.renderImage(4)}
-		    {this.renderImage(5)}
-		    {this.renderImage(6)}
-		    {this.renderImage(7)}
-		    {this.renderImage(8)}
+		    {this.renderLeg(0)}
+		    {this.renderLeg(1)}
+		    {this.renderLeg(2)}
+		    {this.renderLeg(3)}
+		    {this.renderLeg(4)}
+		    {this.renderLeg(5)}
+		    {this.renderLeg(6)}
+		    {this.renderLeg(7)}
+		    {this.renderLeg(8)}
+		</div>
+	    </div>
+	);
+    }
+}
+
+class Head_Choice extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+	index: Array(9).fill(null),
+	head_url: [head_1,head_2,head_3,head_4,head_5,head_6,head_7,head_8,head_9],
+	mark: 'X',
+	last: -1,
+	
+    };
+  }
+  handleClick(i){
+      const ind = this.state.index.slice();
+      if(ind[i]){
+	  ind[i]=null;
+	  this.setState({index:ind, mark: this.state.mark, last: -1})
+	  return;
+      }
+      if(this.state.last!=-1){return;}
+      ind[i]=this.state.mark;
+      this.setState({index:ind, mark: this.state.mark, last: 0});
+      
+  }
+
+    renderHead(i) {
+	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.head_url[i]}/>);
+    }
+    render() {
+	const status = "Choose from below";
+	const head = "Heads: "
+	return (
+	    <div>
+		<div className="status">{status}</div>
+		<div className="title">{head}</div>
+		<div className="choice-row">
+		    {this.renderHead(0)}
+		    {this.renderHead(1)}
+		    {this.renderHead(2)}
+		    {this.renderHead(3)}
+		    {this.renderHead(4)}
+		    {this.renderHead(5)}
+		    {this.renderHead(6)}
+		    {this.renderHead(7)}
+		    {this.renderHead(8)}
+		</div>
+	    </div>
+	);
+    }
+}
+
+class Body_Choice extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+	index: Array(9).fill(null),
+	body_url: [body_1,body_2,body_3,body_4,body_5,body_6,body_7,body_8,body_9],
+	mark: 'X',
+	last: -1,
+	
+    };
+  }
+  handleClick(i){
+      const ind = this.state.index.slice();
+      if(ind[i]){
+	  ind[i]=null;
+	  this.setState({index:ind, mark: this.state.mark, last: -1})
+	  return;
+      }
+      if(this.state.last!=-1){return;}
+      ind[i]=this.state.mark;
+      this.setState({index:ind, mark: this.state.mark, last: 0});
+      
+  }
+
+    renderBody(i) {
+	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.body_url[i]}/>);
+    }
+    render() {
+	const status = "Choose from below";
+	const body = "Bodys: "
+	return (
+	    <div>
+		<div className="status">{status}</div>
+		<div className="title">{body}</div>
+		<div className="choice-row">
+		    {this.renderBody(0)}
+		    {this.renderBody(1)}
+		    {this.renderBody(2)}
+		    {this.renderBody(3)}
+		    {this.renderBody(4)}
+		    {this.renderBody(5)}
+		    {this.renderBody(6)}
+		    {this.renderBody(7)}
+		    {this.renderBody(8)}
+		</div>
+	    </div>
+	);
+    }
+}
+
+
+class Tail_Choice extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+	index: Array(9).fill(null),
+	tail_url: [tail_1,tail_2,tail_3,tail_4,tail_5,tail_6,tail_7,tail_8,tail_9],
+	mark: 'X',
+	last: -1,
+	
+    };
+  }
+  handleClick(i){
+      const ind = this.state.index.slice();
+      if(ind[i]){
+	  ind[i]=null;
+	  this.setState({index:ind, mark: this.state.mark, last: -1})
+	  return;
+      }
+      if(this.state.last!=-1){return;}
+      ind[i]=this.state.mark;
+      this.setState({index:ind, mark: this.state.mark, last: 0});
+      
+  }
+
+    renderTail(i) {
+	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.tail_url[i]}/>);
+    }
+    render() {
+	const status = "Choose from below";
+	const tail = "Tails: "
+	return (
+	    <div>
+		<div className="status">{status}</div>
+		<div className="title">{tail}</div>
+		<div className="choice-row">
+		    {this.renderTail(0)}
+		    {this.renderTail(1)}
+		    {this.renderTail(2)}
+		    {this.renderTail(3)}
+		    {this.renderTail(4)}
+		    {this.renderTail(5)}
+		    {this.renderTail(6)}
+		    {this.renderTail(7)}
+		    {this.renderTail(8)}
 		</div>
 	    </div>
 	);
@@ -82,19 +265,16 @@ class Choice extends React.Component {
 class Build extends React.Component {
   render() {
     return (
-      <div className="build">
-        <div className="build-choice">
-          <Choice />
-        </div>
-        <div className="build-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+	<div className="build">
+            <div className="build-choice">
+		<Leg_Choice />
+		<Head_Choice />
+		<Body_Choice />
+            </div>
+	</div>
     );
   }
 }
-
 
 export function BuildYourOwn(){
     const navigate = useNavigate();
