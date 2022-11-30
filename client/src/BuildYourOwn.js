b7504cbcba8566ce5ae16f8b3a9edda2c5558d42
Jinyuan Zhang <martinzhang0314@163.com>
2022-11-29 21:24:43 -0800

    This version of the code I changed the build your own function. I used one array to store all parts of dinosaurs, and used one class component to define the entire choice page instead of four. This allows me to check choices easier in the future.

--- client/src/BuildYourOwn.js
+++ client/src/BuildYourOwn.js
@@ -61,12 +61,12 @@ function Image(props) {
   );
 }
 
-class Leg_Choice extends React.Component {
+class Choice extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-	index: Array(9).fill(null),
-	leg_url: [leg_1,leg_2,leg_3,leg_4,leg_5,leg_6,leg_7,leg_8,leg_9],
+	index: Array(36).fill(null),
+	url: [leg_1,leg_2,leg_3,leg_4,leg_5,leg_6,leg_7,leg_8,leg_9,head_1,head_2,head_3,head_4,head_5,head_6,head_7,head_8,head_9,body_1,body_2,body_3,body_4,body_5,body_6,body_7,body_8,body_9,tail_1,tail_2,tail_3,tail_4,tail_5,tail_6,tail_7,tail_8,tail_9],
 	mark: 'X',
 	last: -1,
 	
@@ -79,197 +79,87 @@ class Leg_Choice extends React.Component {
 	  this.setState({index:ind, mark: this.state.mark, last: -1})
 	  return;
       }
-      if(this.state.last!=-1){return;}
+      let temp=Math.floor(i/9);
+      console.log(temp);
+      if(this.state.last!=-1 && temp == Math.floor(this.state.last/9)){return;}
       ind[i]=this.state.mark;
-      this.setState({index:ind, mark: this.state.mark, last: 0});
+      this.setState({index:ind, mark: this.state.mark, last: i});
       
   }
 
-    renderLeg(i) {
-	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.leg_url[i]}/>);
+    renderChoice(i) {
+	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.url[i]}/>);
     }
     render() {
 	const status = "Choose from below";
-	const leg = "Legs: "
+	const leg = "Legs: ";
+	const head = "Heads: ";
+	const body = "Bodys: ";
+	const tail = "Tails: ";
+	
 	return (
 	    <div>
 		<div className="status">{status}</div>
 		<div className="title">{leg}</div>
 		<div className="choice-row">
-		    {this.renderLeg(0)}
-		    {this.renderLeg(1)}
-		    {this.renderLeg(2)}
-		    {this.renderLeg(3)}
-		    {this.renderLeg(4)}
-		    {this.renderLeg(5)}
-		    {this.renderLeg(6)}
-		    {this.renderLeg(7)}
-		    {this.renderLeg(8)}
+		    {this.renderChoice(0)}
+		    {this.renderChoice(1)}
+		    {this.renderChoice(2)}
+		    {this.renderChoice(3)}
+		    {this.renderChoice(4)}
+		    {this.renderChoice(5)}
+		    {this.renderChoice(6)}
+		    {this.renderChoice(7)}
+		    {this.renderChoice(8)}
 		</div>
-	    </div>
-	);
-    }
-}
-
-class Head_Choice extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-	index: Array(9).fill(null),
-	head_url: [head_1,head_2,head_3,head_4,head_5,head_6,head_7,head_8,head_9],
-	mark: 'X',
-	last: -1,
-	
-    };
-  }
-  handleClick(i){
-      const ind = this.state.index.slice();
-      if(ind[i]){
-	  ind[i]=null;
-	  this.setState({index:ind, mark: this.state.mark, last: -1})
-	  return;
-      }
-      if(this.state.last!=-1){return;}
-      ind[i]=this.state.mark;
-      this.setState({index:ind, mark: this.state.mark, last: 0});
-      
-  }
-
-    renderHead(i) {
-	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.head_url[i]}/>);
-    }
-    render() {
-	const status = "Choose from below";
-	const head = "Heads: "
-	return (
-	    <div>
-		<div className="status">{status}</div>
 		<div className="title">{head}</div>
 		<div className="choice-row">
-		    {this.renderHead(0)}
-		    {this.renderHead(1)}
-		    {this.renderHead(2)}
-		    {this.renderHead(3)}
-		    {this.renderHead(4)}
-		    {this.renderHead(5)}
-		    {this.renderHead(6)}
-		    {this.renderHead(7)}
-		    {this.renderHead(8)}
+		    {this.renderChoice(9)}
+		    {this.renderChoice(10)}
+		    {this.renderChoice(11)}
+		    {this.renderChoice(12)}
+		    {this.renderChoice(13)}
+		    {this.renderChoice(14)}
+		    {this.renderChoice(15)}
+		    {this.renderChoice(16)}
+		    {this.renderChoice(17)}
 		</div>
-	    </div>
-	);
-    }
-}
-
-class Body_Choice extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-	index: Array(9).fill(null),
-	body_url: [body_1,body_2,body_3,body_4,body_5,body_6,body_7,body_8,body_9],
-	mark: 'X',
-	last: -1,
-	
-    };
-  }
-  handleClick(i){
-      const ind = this.state.index.slice();
-      if(ind[i]){
-	  ind[i]=null;
-	  this.setState({index:ind, mark: this.state.mark, last: -1})
-	  return;
-      }
-      if(this.state.last!=-1){return;}
-      ind[i]=this.state.mark;
-      this.setState({index:ind, mark: this.state.mark, last: 0});
-      
-  }
-
-    renderBody(i) {
-	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.body_url[i]}/>);
-    }
-    render() {
-	const status = "Choose from below";
-	const body = "Bodys: "
-	return (
-	    <div>
-		<div className="status">{status}</div>
 		<div className="title">{body}</div>
 		<div className="choice-row">
-		    {this.renderBody(0)}
-		    {this.renderBody(1)}
-		    {this.renderBody(2)}
-		    {this.renderBody(3)}
-		    {this.renderBody(4)}
-		    {this.renderBody(5)}
-		    {this.renderBody(6)}
-		    {this.renderBody(7)}
-		    {this.renderBody(8)}
+		    {this.renderChoice(18)}
+		    {this.renderChoice(19)}
+		    {this.renderChoice(20)}
+		    {this.renderChoice(21)}
+		    {this.renderChoice(22)}
+		    {this.renderChoice(23)}
+		    {this.renderChoice(24)}
+		    {this.renderChoice(25)}
+		    {this.renderChoice(26)}
 		</div>
-	    </div>
-	);
-    }
-}
-
-
-class Tail_Choice extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-	index: Array(9).fill(null),
-	tail_url: [tail_1,tail_2,tail_3,tail_4,tail_5,tail_6,tail_7,tail_8,tail_9],
-	mark: 'X',
-	last: -1,
-	
-    };
-  }
-  handleClick(i){
-      const ind = this.state.index.slice();
-      if(ind[i]){
-	  ind[i]=null;
-	  this.setState({index:ind, mark: this.state.mark, last: -1})
-	  return;
-      }
-      if(this.state.last!=-1){return;}
-      ind[i]=this.state.mark;
-      this.setState({index:ind, mark: this.state.mark, last: 0});
-      
-  }
-
-    renderTail(i) {
-	return (<Image value={this.state.index[i]} onClick={() => this.handleClick(i)} url={this.state.tail_url[i]}/>);
-    }
-    render() {
-	const status = "Choose from below";
-	const tail = "Tails: "
-	return (
-	    <div>
-		<div className="status">{status}</div>
 		<div className="title">{tail}</div>
 		<div className="choice-row">
-		    {this.renderTail(0)}
-		    {this.renderTail(1)}
-		    {this.renderTail(2)}
-		    {this.renderTail(3)}
-		    {this.renderTail(4)}
-		    {this.renderTail(5)}
-		    {this.renderTail(6)}
-		    {this.renderTail(7)}
-		    {this.renderTail(8)}
+		    {this.renderChoice(27)}
+		    {this.renderChoice(28)}
+		    {this.renderChoice(29)}
+		    {this.renderChoice(30)}
+		    {this.renderChoice(31)}
+		    {this.renderChoice(32)}
+		    {this.renderChoice(33)}
+		    {this.renderChoice(34)}
+		    {this.renderChoice(35)}
 		</div>
 	    </div>
 	);
     }
 }
+
 
 class Build extends React.Component {
   render() {
     return (
 	<div className="build">
             <div className="build-choice">
-		<Leg_Choice />
-		<Head_Choice />
-		<Body_Choice />
+		<Choice />
             </div>
 	</div>
     );
